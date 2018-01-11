5b256ff9ec2f37e8783c2ce57b8ab748f5a23bc4
trvinhlong <longtv0910@gmail.com>
2018-01-11 23:51:40 +0700

    add FB and phone

--- src/components/App/Header.js
+++ src/components/App/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from '../../logo.jpg';
+import logo from '../../logo.png';
 
 const Header = () => (
     <header className="App-header">
         <img id="logo" src={logo} className="App-logo" alt="Tanks.vn logo"/>
-        <h1 className="App-title">Tanks.vn</h1>
+        <div className="App-title">Tanks.vn</div>
+        <div className="App-intro">FB page: <a href='http://facebook.com/tanksvn'>http://facebook.com/tanksvn</a> - Hotline: 0125.737.9966</div>
     </header>
 )
 
