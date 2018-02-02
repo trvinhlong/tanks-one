b81bca13bc7bf32b2b6e56f4819969f27693cd84
Truong Vinh Long <long_tv@septeni-technology.jp>
2018-02-02 16:51:29 +0700

    add script to auto sync image

--- cronjob/syncImage.js
+++ cronjob/syncImage.js
@@ -3,16 +3,17 @@ var cron = require('cron');
 var request = require('request');
 
 var job = new cron.CronJob({
-  cronTime: '45 * * * *',
+  cronTime: '55 * * * *',
   onTick: function() {
     
 	request('http://tanks.vn/facebook/login', function (error, response, body) {
-
+        console.log(response);
 	});
   },
   start: false,
   timeZone: 'America/Los_Angeles'
 });
 
+console.log('job started');
 job.start();
 
